238496391d98492be29efefedb89a7e5003fcf5e
David Chu <dabidjohn424@gmail.com>
2025-11-04 21:22:12 +0800

    fix: update case sensitivity in search input logic

--- js/logic.js
+++ js/logic.js
@@ -56,6 +56,7 @@ export function countCartItems(cart) {
 export function searchProductItems(input, products) {
   let search = products.filter((product) => {
     const title = product.title.toLowerCase();
+    input = input.toLowerCase();
     return title.includes(input);
   });
 
